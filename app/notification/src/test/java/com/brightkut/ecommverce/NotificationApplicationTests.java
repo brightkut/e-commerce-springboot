133d495fc4ba7e6de9638dd34b439a24bac7cbb2
brightkut <dsorn2@gmail.com>
2024-06-01 17:46:02 +0700

    feat: add notification service

---
+++ app/notification/src/test/java/com/brightkut/ecommverce/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brightkut.ecommverce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
