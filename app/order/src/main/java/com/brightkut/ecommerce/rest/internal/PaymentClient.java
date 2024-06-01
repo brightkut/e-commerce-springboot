f4c540c2f3c99962a37c5e6e24e88f284a158daa
brightkut <dsorn2@gmail.com>
2024-06-01 17:35:10 +0700

    feat: implement payment service

---
+++ app/order/src/main/java/com/brightkut/ecommerce/rest/internal/PaymentClient.java
@@ -0,0 +1,15 @@
+package com.brightkut.ecommerce.rest.internal;
+
+import com.brightkut.ecommerce.rest.internal.model.PaymentRequest;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(
+        name = "payment-service",
+        url = "${application.config.payment-url}"
+)
+public interface PaymentClient {
+    @PostMapping
+    Integer requestOrderPayment(@RequestBody PaymentRequest request);
+}
